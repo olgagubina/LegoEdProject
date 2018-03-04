84f38b2b4ee1f4682390f3754ff1f5e0dd183fa9
olgagubina <oagubina@gmail.com>
2018-03-04 13:43:25 +0200

    server is working

---
+++ routes/points-routes.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET point items */
+router.get('/', function(req, res, next) {
+    res.send('here is the points page')
+});
+
+/* ADD point item */
+
+/* UPDATE point item */
+
+/* DELETE point item */
+
+module.exports = router;
---
+++ routes/students-routes.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET students */
+router.get('/', function(req, res, next) {
+    res.send('here is the students page')
+});
+  
+/* ADD student */
+
+/* UPDATE  student */
+
+/* DELETE  student */
+
+module.exports = router;
