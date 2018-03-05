918c39175cd94720d81270d978356e72cf72e097
olgagubina <oagubina@gmail.com>
2018-03-05 23:15:20 +0200

    added Procfile

--- routes/students-routes.js
+++ routes/students-routes.js
@@ -2,18 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    // host: 'localhost',
+var connection; 
+
+var localConnection = {
+   user: 'root', // < MySQL username >
+    // password: '1234', // < MySQL password COOKIE and MC >
+    // password: 'easyPass', // < MySQL password ANNA>
+    password: '147258', // < MySQL password OLGA>
+    database: 'lego' // <your database name>
+}
+var clearDBConnection = {
     host: 'us-cdbr-iron-east-05.cleardb.net',
     user: 'bbbb8310aa5c1c',
     password: 'f64edb0b',
     database: 'heroku_365eb437c5f937e'
-    // user: 'root', // < MySQL username >
-    // password: '1234', // < MySQL password >
-    // password: 'easyPass', // < MySQL password >
-    // password: '147258', // < MySQL password >
-    // database: 'lego' // <your database name>
-});
+ }
+
+ if(process.env.PORT === 3000) {
+    connection = mysql.createConnection(localConnection);
+ }
+ else {
+    connection = mysql.createConnection(clearDBConnection);
+ }
+// connection = mysql.createConnection({
+//     // host: 'localhost',
+//     host: 'us-cdbr-iron-east-05.cleardb.net',
+//     user: 'bbbb8310aa5c1c',
+//     password: 'f64edb0b',
+//     database: 'heroku_365eb437c5f937e'
+//     // user: 'root', // < MySQL username >
+//     // password: '1234', // < MySQL password >
+//     // password: 'easyPass', // < MySQL password >
+//     // password: '147258', // < MySQL password >
+//     // database: 'lego' // <your database name>
+// });
 
 // GET ALL students
 router.get('/all', (req, res) => {
