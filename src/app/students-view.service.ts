28b5318cf660017d33975b3e1da172bfcdb3848f
olgagubina <oagubina@gmail.com>
2018-03-07 19:37:40 +0200

    points edit

--- src/app/students-view.service.ts
+++ src/app/students-view.service.ts
@@ -22,6 +22,7 @@ export class StudentsViewService {
   prizes: PointItem[];
   penalties: PointItem[];
   rewards: PointItem[];
+  show: boolean = true;
 
   constructor(private http: HttpClient) { }
 
@@ -131,6 +132,12 @@ export class StudentsViewService {
         this.displayPointsData$.next(data)
       }
     );
+  }
+
+  //UPDATE POINT ITEM 
+  updPointItem(updPointItem:PointItem): Observable<Student> {
+    console.log(updPointItem);
+    return this.http.put<Student>('api/points/update/'+updPointItem.pointId, updPointItem);
   }
 
   // SAVE TRANSACTIONS
