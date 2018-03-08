3a45f12e8f4e7b1c10266c1a37cd7a6a9059db00
olgagubina <oagubina@gmail.com>
2018-03-08 16:22:46 +0200

    clearDB is on

--- routes/students-routes.js
+++ routes/students-routes.js
@@ -35,8 +35,8 @@ var amazonDBConnection = {
 
 
 //DB SWITCHER
-sqlConnection = mysql.createConnection(amazonDBConnection);
-// sqlConnection = mysql.createConnection(clearDBConnection);
+// sqlConnection = mysql.createConnection(amazonDBConnection);
+sqlConnection = mysql.createConnection(clearDBConnection);
 // sqlConnection = mysql.createConnection(localConnection);
 
 // FANCY FUNC TO MAKE CONNECTION (local OR heroku)
